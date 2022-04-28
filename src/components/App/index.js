3025fb2fd30ed9aab6d8ecd07246f818fbe2b376
peter-robert-cole <peter7cole@gmail.com>
2022-04-27 23:43:42 -0700

    fix(react): a couple errors for react 18

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
 const App = () => {
-  return (
-    <h1 class="text-3xl font-bold underline">Analytics Dashboard</h1>
-  )
-}
+  return <h1 className="text-3xl font-bold underline">Analytics Dashboard</h1>;
+};
 
 export default App;
